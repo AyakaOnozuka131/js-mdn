add6e626a2ee701e36811bdc95ff904dce1d446a
AyakaOnozuka131 <c_yu_again@icloud.com>
2022-02-16 12:43:26 +0900

    update : Add loop function

--- js-object-building-practice/main.js
+++ js-object-building-practice/main.js
@@ -35,20 +35,20 @@ Ball.prototype.draw = function() {
   ctx.fill();
 }
 
-Ball.prototype.update() = function() {
-  if((this.x + this.size) >= width) {
+Ball.prototype.update = function() {
+  if ((this.x + this.size) >= width) {
     this.velX = -(this.velX);
   }
 
-  if((this.x + this.size) <= 0) {
+  if ((this.x - this.size) <= 0) {
     this.velX = -(this.velX);
   }
 
-  if((this.y + this.size) >= height) {
+  if ((this.y + this.size) >= height) {
     this.velY = -(this.velY);
   }
 
-  if((this.y + this.size) <= 0) {
+  if ((this.y - this.size) <= 0) {
     this.velY = -(this.velY);
   }
 
@@ -56,10 +56,45 @@ Ball.prototype.update() = function() {
   this.y += this.velY;
 }
 
+Ball.prototype.collisionDetect = function() {
+  for (let j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+      }
+    }
+  }
+}
+
 let balls = [];
 
 while (balls.length < 25) {
-  
+  let size = random(10, 20);
+  let ball = new Ball(
+    random(0 + size,width - size),
+    random(0 + size,height - size),
+    random(-7,7),
+    random(-7,7),
+    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+    size
+  );
+  balls.push(ball)
 }
 
-let testBall = new Ball(50, 100, 4, 4, 'blue', 10);
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
